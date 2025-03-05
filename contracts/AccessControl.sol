b5efcae4c9d8d86e2657932eeb3ac6f3c0d6e405
Harish G <harishgovind6000@gmail.com>
2025-03-05 15:25:08 +0530

    updated AccessControl

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -2,76 +2,109 @@
 pragma solidity ^0.8.0;
 
 contract AccessControlContract {
-    enum AccessType {Read, Write, Both, NoAccess}
+    enum AccessType {NoAccess, Read, Write, Both}
     struct ValidationInfo {
         AccessType accessType;
         bool isAll;
         uint deadline;
         string[] keys;
-        mapping (string => uint) accessList;
+        uint lastCleanUp;
     }
-    mapping (address => mapping(string => ValidationInfo)) public permissionList;
 
-    function grantAccess(string memory uid, AccessType accessType,bool isAll, string[] memory accessList,uint deadline)public returns(bool){
+    mapping (address => mapping (address => mapping (string=>uint))) private accessList;
+    mapping (address => mapping(address => ValidationInfo)) private permissionList;
+
+    event AccessGranted(address indexed patient, address hospitalAddress, AccessType accessType, bool isAll, string[] keys, uint deadline);
+    event AccessRemoved(address indexed patient, address hospitalAddress, string[] keys);
+    event ExpiredKeysCleaned(address indexed patient, address hospitalAddress, uint numKeysRemoved);
+    event AllAccessRemoved(address indexed patient, address hospitalAddress);
+
+    function grantAccess(address hospitalAddress, AccessType accessType,bool isAll, string[] memory fileList,uint deadline)public returns(bool){
         
-        ValidationInfo storage info = permissionList[msg.sender][uid];
+        ValidationInfo storage info = permissionList[msg.sender][hospitalAddress];
         info.accessType=accessType;
         info.isAll=isAll;
         info.deadline=block.timestamp+deadline;
 
+        if(info.lastCleanUp==0){
+            info.lastCleanUp=block.timestamp;
+        }
+
         if (!isAll){
-            for (uint i=0; i<accessList.length;i++){
-                if(info.accessList[accessList[i]]==0){
-                    info.keys.push(accessList[i]);
+            for (uint i=0; i<fileList.length;i++){
+                if(accessList[msg.sender][hospitalAddress][fileList[i]]==0){
+                    info.keys.push(fileList[i]);
                 }
-                info.accessList[accessList[i]]=block.timestamp + deadline;
+                accessList[msg.sender][hospitalAddress][fileList[i]]=block.timestamp + deadline;
             }
-        }   
+        }
+        emit AccessGranted(msg.sender, hospitalAddress, accessType, isAll, fileList, info.deadline);
+        if (info.lastCleanUp<block.timestamp-100 days){
+            cleanupExpiredKeys(hospitalAddress);
+            info.lastCleanUp=block.timestamp;
+        }
         return true;
     }
 
-    function getAccessList(string memory uid)public view returns(string memory){
-        ValidationInfo storage info = permissionList[msg.sender][uid];
-        string memory t="";
-        for(uint i=0; i<info.keys.length;i++){
-            t = string(abi.encodePacked(t, info.keys[i], "-", uint2str(info.accessList[info.keys[i]]), ";"));
+    function getAccessList(address hospitalAddress) public view returns (string[] memory keys, uint[] memory deadlines) {
+        ValidationInfo storage info = permissionList[msg.sender][hospitalAddress];
+        uint[] memory deadlinesArray = new uint[](info.keys.length);
+        for (uint i = 0; i < info.keys.length; i++) {
+            deadlinesArray[i] = accessList[msg.sender][hospitalAddress][info.keys[i]];
         }
-        return t;
+        return (info.keys, deadlinesArray);
     }
 
-    function removeAccess(string memory uid, string[] memory list)public {
-        ValidationInfo storage info = permissionList[msg.sender][uid];
+    function removeAccess(address hospitalAddress, string[] memory list)public {
+        ValidationInfo storage info = permissionList[msg.sender][hospitalAddress];
         for(uint i=0;i<list.length;i++){
-            if(info.accessList[list[i]]!=0){
-                info.accessList[list[i]]=block.timestamp;
+            if(accessList[msg.sender][hospitalAddress][list[i]]!=0){
+                accessList[msg.sender][hospitalAddress][list[i]]=block.timestamp;
             }
         }
+        if (info.lastCleanUp<block.timestamp-100 days){
+            cleanupExpiredKeys(hospitalAddress);
+            info.lastCleanUp=block.timestamp;
+        }
+        emit AccessRemoved(msg.sender, hospitalAddress, list);
     }
 
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
+    function removeAllAcess(address hospitalAddress)public{
+        ValidationInfo storage info=permissionList[msg.sender][hospitalAddress];
+        require(info.deadline > 0, "No access permissions exist for this hospital");
+        for (uint i = 0; i < info.keys.length; i++) {
+            string memory key = info.keys[i];
+            delete accessList[msg.sender][hospitalAddress][key]; // Clear each entry
         }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len;
-        while (_i != 0) {
-            bstr[--k] = bytes1(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
+        emit AllAccessRemoved(msg.sender, hospitalAddress);
+        delete permissionList[msg.sender][hospitalAddress];
     }
 
-    function getAccessType(string memory uid)public view returns (uint){
-        ValidationInfo storage info = permissionList[msg.sender][uid];
+    function getAccessType(address hospitalAddress)public view returns (uint){
+        ValidationInfo storage info = permissionList[msg.sender][hospitalAddress];
         return uint(info.accessType);
     }
 
-    
+    function cleanupExpiredKeys(address hospitalAddress) public {
+        ValidationInfo storage info = permissionList[msg.sender][hospitalAddress];
+        uint i = 0;
+        uint numRemoved = 0;
+        while (i < info.keys.length) {
+            string memory key = info.keys[i];
+            if (accessList[msg.sender][hospitalAddress][key] <= block.timestamp) {
+                // Swap-and-pop to remove the key
+                info.keys[i] = info.keys[info.keys.length - 1];
+                info.keys.pop();
+                delete accessList[msg.sender][hospitalAddress][key]; // Clear storage
+                numRemoved++;
+            } else {
+                i++;
+            }
+        }
+        if (numRemoved > 0) {
+            emit ExpiredKeysCleaned(msg.sender, hospitalAddress, numRemoved);
+        }
+    }
+
 }
 
